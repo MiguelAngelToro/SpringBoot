bb5c45b393af63ade20a2fab0d70e950b95140e7
MiguelAngelToro <149112851+MiguelAngelToro@users.noreply.github.com>
2024-04-19 13:33:51 -0500

    Add files via upload

---
+++ ejercicio01/DB TAREAS.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE db_to_do_list;
+desc tarea;
+
+insert into tarea (creation_date,creation_time,description,status,title) values ("2024-04-19","12:32:00","Terminar taller Spring Boot","Activo","Taller Spring Boot");
+insert into coder (age,clan,nombre) values (22,"Lovelace","Maria Guadalupe");
+
+drop table coder;
